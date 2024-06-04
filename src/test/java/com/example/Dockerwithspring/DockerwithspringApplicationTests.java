1bd167f38af045b7e1f2660eb13b062fcd7b3232
HireResidency <residencyhira@gmail.com>
2024-06-04 19:11:31 +0530

    New Application

---
+++ src/test/java/com/example/Dockerwithspring/DockerwithspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dockerwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerwithspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
